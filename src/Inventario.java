8aaa255746afd39b31508e48d1fb41c31226052c
susan <susan.jj.michelle@gmail.com>
2026-03-03 16:53:52 +0100

    q

---
+++ src/Inventario.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventario {
+//atributo privado 
+	///en primero queria crear un ArrayList<Mascotas>
+	//private ArrayList<Mascotas> listaMacotas= new ArrayList<>();
+	private List<Mascotas> ListaMascotas;
+	
+	public Inventario() {
+		ListaMascotas = new ArrayList<>();
+	}
+	//vaciar() para haci borrar todo 
+	public void vaciar() {
+		ListaMascotas.clear();
+	}
+	
+	//metodo para insertar animales
+	
+	//metodo para insertar eliminar animlaes
+	
+	//imprimir los nombres de los animales
+	
+	
+	
+}
