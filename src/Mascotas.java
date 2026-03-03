31918be0a664073adebf20d009aab6c3f9de9ab2
susan <susan.jj.michelle@gmail.com>
2026-03-03 16:31:16 +0100

    1

--- src/Mascotas.java
+++ src/Mascotas.java
@@ -5,6 +5,30 @@ public abstract class Mascotas {
 	protected int edad;
 	protected String estado;
 	protected String fechaNacimiento;
-	
 
-}
+	//constructores 
+	public Mascotas(String nombreI, int edad, String estado, String fechaNacimiento) {
+		this.nombreI = nombreI;
+		this.edad = edad;
+		this.estado = estado;
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	//getters necesarios 
+
+	public String getNombreI() {
+		return nombreI;
+	}
+
+	public void setNombreI(String nombreI) {
+		this.nombreI = nombreI;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	}
