5f43f4ffce7f8f3a61a795bab10f59b29142446a
susan <susan.jj.michelle@gmail.com>
2026-03-03 16:26:36 +0100

    1

--- src/Mascotas.java
+++ src/Mascotas.java
@@ -1,5 +1,10 @@
-
+//@autor:Susan isla isla
 public class Mascotas {
 //clase Abstacta
+	protected String nombreI;
+	protected int edad;
+	protected String estado;
+	protected String fechaNacimiento;
+	
 
 }
