d7705a5785be050ada445c8cd9e72829656a8d2f
susan <susan.jj.michelle@gmail.com>
2026-03-03 17:29:03 +0100

    1

--- src/Test1.java
+++ src/Test1.java
@@ -3,8 +3,11 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// @autor:Susan isla isla
+		//perro
 		Perro r = new Perro("Rocky", 1, "OK", "11022017");
+		//gato
 		Gato g = new Gato("Negro", 2, "OK", "05012016");
+		//ejecucion de test
 		System.out.println(r.getNombreI());
 		System.out.println(g.getEstado());
 		Inventario i = new Inventario();
